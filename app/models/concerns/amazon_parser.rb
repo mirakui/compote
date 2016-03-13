ca7f404f1c65235f82febb8442b029142c02b9ce
Issei Naruta <naruta@cookpad.com>
2016-03-13 15:03:12 +0900

    Implement AmazonParser

---
+++ app/models/concerns/amazon_parser.rb
@@ -0,0 +1,47 @@
+require 'nokogiri'
+
+module AmazonParser
+  extend ActiveSupport::Concern
+
+  included do
+
+    def parse_body_xml
+      result = {}
+
+      item = item_by_type('ABIS_BOOK')
+      result[:book] = parse_item_node item if item
+
+      item = item_by_type('ABIS_EBOOKS')
+      result[:ebook] = parse_item_node item if item
+
+      result
+    end
+
+    def body_xml
+      @body_xml ||= Nokogiri::XML self.body
+    end
+
+    private
+
+    def item_by_type(type)
+      body_xml.css('Items Item').find do |x|
+        x.css('ProductTypeName').text == type
+      end
+    end
+
+    def parse_item_node(item)
+      result = {}
+
+      result[:authors] = item.css('Author').map(&:text)
+
+      %w[ASIN Publisher Studio Title PublicationDate ReleaseDate].each do |name|
+        key = name.underscore.intern
+        value = item.at_css(name).try(&:text)
+        result[key] = value if value
+      end
+
+      result
+    end
+
+  end
+end
--- app/models/crawled_response.rb
+++ app/models/crawled_response.rb
@@ -1,7 +1,10 @@
 class CrawledResponse < ActiveRecord::Base
+  include AmazonParser
+
   LIFETIME_SEC = 1.day
 
   def expired?
+    return false if Rails.env.development?
     Time.now - self.updated_at > LIFETIME_SEC
   end
 end
