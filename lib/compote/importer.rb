d6906712d234b0e54ff28ee66ea81cf1ea39864e
Issei Naruta <naruta@cookpad.com>
2016-03-19 14:03:04 +0900

    Remove author model

--- lib/compote/importer.rb
+++ lib/compote/importer.rb
@@ -1,10 +1,9 @@
 module Compote
   class Importer
-    BATCH_SIZE = 10
+    BATCH_SIZE = 100
 
     def initialize
-      @publishers = {}
-      @authors = {}
+      @publishers = nil
     end
 
     def start
@@ -15,46 +14,40 @@ module Compote
 
     def import_books
       Source.find_in_batches(batch_size: BATCH_SIZE) do |sources|
-        sources.each do |source|
-          import_source source
+        items = sources.map(&:parse_body_xml)
+        asins = items.map {|v| v.values.map {|x| x[:asin] } }.flatten.compact
+        cached_books = Hash[Book.where(asin: asins).to_a.map {|b| [b.asin, b] }]
+        sources.each_with_index do |source, i|
+          items[i].each do |type, item|
+            import_book_item type, item, source, cached_books
+          end
         end
       end
     end
 
-    def import_source(source)
-      items = source.parse_body_xml
-      items.each do |type, item|
-        import_book_item type, item, source
-      end
-    end
-
-    def import_book_item(type, item, source)
-      ActiveRecord::Base.transaction do
-        book = Book.find_or_create_by asin: item[:asin]
-        book.released_on = item[:release_date]
-        book.published_on = item[:publication_date]
-        book.raw_title = item[:title]
-        book.is_ebook = type == :ebook
-        book.is_adult = item[:is_adult_product].to_i == 1
-        book.publisher = find_publisher_by_name item[:publisher]
-        book.authors = find_authors_by_names item[:authors]
-        book.source = source
-        book.save
+    def import_book_item(type, item, source, cached_books)
+      book = cached_books[item[:asin]] || Book.new(asin: item[:asin])
+      book.released_on = item[:release_date]
+      book.published_on = item[:publication_date]
+      book.raw_title = item[:title]
+      book.is_ebook = type == :ebook
+      book.is_adult = item[:is_adult_product].to_i == 1
+      book.publisher = find_publisher_by_name item[:publisher]
+      book.authors = item[:authors].join("\t")
+      book.source = source
+      if book.changed?
+        Book.transaction do
+          book.save
+        end
       end
     rescue => e
+        binding.pry
       Rails.logger.error "import failed: { #{type}: #{item.inspect} } exception: #{e} (#{e.class})"
     end
 
     def find_publisher_by_name(name)
-      @publishers[name] ||= Publisher.find_or_create_by name: name
-    end
-
-    def find_authors_by_names(names)
-      names.map {|name| find_author_by_name name }
-    end
-
-    def find_author_by_name(name)
-      @authors[name] ||= Author.find_or_create_by name: name
+      @publishers ||= Hash[Publisher.all.to_a.map {|x| [x.name, x] }]
+      @publishers[name] ||= Publisher.new(name: name)
     end
   end
 end
