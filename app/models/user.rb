f4e4070edf8bfffc1424621261f3492227fb6d1f
Issei Naruta <naruta@cookpad.com>
2016-03-28 22:54:49 +0900

    Localized validation message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_format_of :email, with: /\A\s*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\s*\z/i
+  validates_uniqueness_of :email
+  validates_length_of :password, minimum: 6
+  before_save :strip_email
 
-  validates_presence_of :email
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  private
+  def strip_email
+    self.email.strip! if self.email
+  end
 end
